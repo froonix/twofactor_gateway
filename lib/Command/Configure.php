9f09f7887bbfe5bed6d6270769ebc5988cde337c
Christian Schrötter <cs@fnx.li>
2020-07-03 12:33:59 +0200

    Implement portal.clickatell.com API
    Signed-off-by: Christian Schrötter <cs@fnx.li>

--- lib/Command/Configure.php
+++ lib/Command/Configure.php
@@ -38,6 +38,7 @@ use OCA\TwoFactorGateway\Service\Gateway\SMS\Provider\PuzzelSMSConfig;
 use OCA\TwoFactorGateway\Service\Gateway\SMS\Provider\HuaweiE3531Config;
 use OCA\TwoFactorGateway\Service\Gateway\SMS\Provider\SpryngSMSConfig;
 use OCA\TwoFactorGateway\Service\Gateway\SMS\Provider\ClickatellCentralConfig;
+use OCA\TwoFactorGateway\Service\Gateway\SMS\Provider\ClickatellPortalConfig;
 use OCA\TwoFactorGateway\Service\Gateway\Telegram\Gateway as TelegramGateway;
 use OCA\TwoFactorGateway\Service\Gateway\Telegram\GatewayConfig as TelegramConfig;
 use Symfony\Component\Console\Command\Command;
@@ -108,7 +109,7 @@ class Configure extends Command {
 	private function configureSms(InputInterface $input, OutputInterface $output) {
 		$helper = $this->getHelper('question');
 
-		$providerQuestion = new Question('Please choose a SMS provider (websms, playsms, clockworksms, puzzelsms, ecallsms, voipms, huawei_e3531, spryng, sms77io, ovh, clickatellcentral', 'websms');
+		$providerQuestion = new Question('Please choose a SMS provider (websms, playsms, clockworksms, puzzelsms, ecallsms, voipms, huawei_e3531, spryng, sms77io, ovh, clickatellcentral, clickatellportal): ', 'websms');
 		$provider = $helper->ask($input, $output, $providerQuestion);
 
 		/** @var SMSConfig $config */
@@ -297,6 +298,17 @@ class Configure extends Command {
 				$providerConfig->setApi($api);
 				$providerConfig->setUser($username);
 				$providerConfig->setPassword($password);
+				break;
+
+			case 'clickatellportal':
+				$config->setProvider($provider);
+				/** @var ClickatellPortalConfig $providerConfig */
+				$providerConfig = $config->getProvider()->getConfig();
+
+				$apiQuestion = new Question('Please enter your portal.clickatell.com API-Key: ');
+				$apiKey = $helper->ask($input, $output, $apiQuestion);
+
+				$providerConfig->setApiKey($apiKey);
 				break;
 
 			default:
